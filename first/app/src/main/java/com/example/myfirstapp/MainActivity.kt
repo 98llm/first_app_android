b1d6fc7d0875b65117da9971c5638912e5d10585
98llm <limamonteiro2010@hotmail.com>
2021-03-17 17:58:34 -0300

    first

---
+++ first/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun login(view: View) {
+        val editText = findViewById<EditText>(R.id.edit_user)
+        val editPassword = findViewById<EditText>(R.id.editTextPassword)
+        val password = editPassword.text.toString()
+        val username = editText.text.toString()
+
+        if( username == "aluno" && password == "impacta") {
+            val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, username)
+            }
+            startActivity(intent)
+        } else {
+            Toast.makeText(this,"Usuário ou senha incorretos", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+    }
+}
