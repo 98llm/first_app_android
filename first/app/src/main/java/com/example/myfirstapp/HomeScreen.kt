851bffd7dd45332a578c66638e35ce717e75f537
98llm <limamonteiro2010@hotmail.com>
2021-03-17 19:03:46 -0300

    end

--- first/app/src/main/java/com/example/myfirstapp/HomeScreen.kt
+++ first/app/src/main/java/com/example/myfirstapp/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.view.Menu
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,14 +14,15 @@ class HomeScreen : AppCompatActivity() {
         // Get the Intent that started this activity and extract the string
         val username = intent.getStringExtra(EXTRA_MESSAGE)
 
-        //setSupportActionBar(findViewById(R.id.my_toolbar))
-
-
-
         Toast.makeText(this,"Bem vindo $username", Toast.LENGTH_SHORT).show()
         // Capture the layout's TextView and set the string as its text
         val textView = findViewById<TextView>(R.id.textView2).apply {
             text = username
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
