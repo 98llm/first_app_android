83e86a1d2f84e7cf3cfc0c888338b57e8fcf0725
98llm <limamonteiro2010@hotmail.com>
2021-03-20 01:34:40 -0300

    a

--- first/app/src/main/java/com/example/myfirstapp/HomeScreen.kt
+++ first/app/src/main/java/com/example/myfirstapp/HomeScreen.kt
@@ -1,7 +1,10 @@
 package com.example.myfirstapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,15 +17,34 @@ class HomeScreen : AppCompatActivity() {
         // Get the Intent that started this activity and extract the string
         val username = intent.getStringExtra(EXTRA_MESSAGE)
 
-        Toast.makeText(this,"Bem vindo $username", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Bem vindo $username", Toast.LENGTH_SHORT).show()
         // Capture the layout's TextView and set the string as its text
         val textView = findViewById<TextView>(R.id.textView2).apply {
             text = username
         }
+
+        supportActionBar?.title = "Inicio"
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.search) {
+        } else if (id == R.id.update) {
+            Toast.makeText(this, "Clicou atualizar", Toast.LENGTH_SHORT).show()
+        } else if (id == R.id.settings) {
+            Toast.makeText(this, "Clicou config", Toast.LENGTH_SHORT).show()
+        } else if(id == R.id.logout) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
--- first/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ first/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -3,9 +3,18 @@ package com.example.myfirstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.util.Log
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
@@ -13,25 +22,56 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    /** Called when the user taps the Send button */
-    fun login(view: View) {
         val editText = findViewById<EditText>(R.id.edit_user)
+        val cardView = findViewById<CardView>(R.id.cardView_login)
+        val layout = findViewById<ConstraintLayout>(R.id.constraint_layout_login)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar2)
+        val textView = findViewById<TextView>(R.id.textView)
+        val intent = Intent(this, HomeScreen::class.java)
+        val username = editText.text.toString()
         val editPassword = findViewById<EditText>(R.id.editTextPassword)
         val password = editPassword.text.toString()
-        val username = editText.text.toString()
 
-        if( username == "aluno" && password == "impacta") {
-            val intent = Intent(this, HomeScreen::class.java).apply {
-                putExtra(EXTRA_MESSAGE, username)
-            }
-            startActivity(intent)
-        } else {
-            Toast.makeText(this,"Usuário ou senha incorretos", Toast.LENGTH_SHORT).show()
+        fun buttonActivated() {
+            progressBar.visibility = View.VISIBLE
+            textView.text = "Aguarde..."
+        }
+
+        fun buttonFinished(username: String) {
+            layout.setBackgroundColor(cardView.resources.getColor(R.color.green))
+            progressBar.visibility = View.GONE
+            textView.text = "Pronto!"
+            startActivity(intent.).apply {
+                putExtra(username)}
         }
 
 
+        finalButton.setOnClickListener {
+            val editPassword = findViewById<EditText>(R.id.editTextPassword)
+            val password = editPassword.text.toString()
 
+            if (username == "aluno" && password == "impacta") {
+
+                Handler().postDelayed({
+
+                    // Chama funcao que finaliza o botão
+                    buttonFinished(username)
+                    //Altera a Activity
+
+                }, 1500)
+                // Chama funcao que Ativa o botão
+                buttonActivated()
+
+                Handler().postDelayed({
+
+                    this.recreate()
+
+                }, 1700)
+            } else {
+                Toast.makeText(this, "Usuário ou senha incorretos", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
