ba86e187e3a185dae29dc0a7bb8540d5b9b421d7
98llm <limamonteiro2010@hotmail.com>
2021-04-14 18:21:48 -0300

    a

--- first/app/src/main/java/com/example/myfirstapp/HomeScreen.kt
+++ first/app/src/main/java/com/example/myfirstapp/HomeScreen.kt
@@ -11,7 +11,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_home_screen.*
 import kotlinx.android.synthetic.main.toolbar.*
@@ -19,6 +22,8 @@ import kotlinx.android.synthetic.main.toolbar.*
 
 
 class HomeScreen : AppCompatActivity(),  NavigationView.OnNavigationItemSelectedListener  {
+    private val context: Context get() = this
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
@@ -34,15 +39,17 @@ class HomeScreen : AppCompatActivity(),  NavigationView.OnNavigationItemSelected
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        //configuraMenuLateral()
+        configuraMenuLateral()
+
+        recyclerDisciplinas?.layoutManager = LinearLayoutManager(context)
+        recyclerDisciplinas?.itemAnimator = DefaultItemAnimator()
+        recyclerDisciplinas?.setHasFixedSize(true)
     }
 
     private fun configuraMenuLateral() {
         // ícone de menu (hamburger) para mostrar o menu
-        var toogle = ActionBarDrawerToggle(
+        var toogle = ActionBarDrawerToggle(this, layoutMenuLateral, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
 
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close)
 
         layoutMenuLateral.addDrawerListener(toogle)
         toogle.syncState()
@@ -53,25 +60,41 @@ class HomeScreen : AppCompatActivity(),  NavigationView.OnNavigationItemSelected
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_disciplinas -> {
-                //Toast.makeText(this, "Clicou disciplinas", Toast.LENGTH_LONG).show()
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle("Teste!")
-                builder.setNeutralButton("ok", null)
-                val dialog = builder.create()
-                dialog.show()
-
+                Toast.makeText(this, "Clicou Disciplinas", Toast.LENGTH_SHORT).show()
             }
+
             R.id.nav_forum -> {
                 Toast.makeText(this, "Clicou Forum", Toast.LENGTH_SHORT).show()
             }
+
+            R.id.nav_localizacao -> {
+                Toast.makeText(this, "Clicou Localização", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
+        // fecha menu depois de tratar o evento
         layoutMenuLateral.closeDrawer(GravityCompat.START)
         return true
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // infla o menu com os botões da ActionBar
         menuInflater.inflate(R.menu.menu_main, menu)
+        // vincular evento de buscar
+        (menu?.findItem(R.id.search)?.actionView as SearchView).setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                // ação enquanto está digitando
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // ação  quando terminou de buscar e enviou
+                return false
+            }
+
+        })
         return true
     }
 
