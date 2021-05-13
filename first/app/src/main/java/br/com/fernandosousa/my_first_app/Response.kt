ad12545d7116cfc75cf3ba26cc6490e5f83b5ed3
LEANDRO L MONTEIRO <60761500+98llm@users.noreply.github.com>
2021-05-12 21:10:47 -0300

    del

--- first/app/src/main/java/br/com/fernandosousa/my_first_app/Response.kt
+++
@@ -1,6 +0,0 @@
-package br.com.fernandosousa.my_first_app
-
-data class Response (val status:String, val msg:String) {
-    // verificar se o chamada foi executada com sucesso
-    fun isOK() = "OK".equals(status, ignoreCase = true)
-}
